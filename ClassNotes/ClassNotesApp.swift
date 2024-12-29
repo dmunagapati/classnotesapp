8e616a78412e8f2b5e8b048de7d1dc8c38e4b9c0
dmunagapati <dmunagapati@gmail.com>
2024-12-28 21:24:35 -0500

    Initial Commit

---
+++ ClassNotes/ClassNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassNotesApp.swift
+//  ClassNotes
+//
+//  Created by Deepthi Munagapati on 12/28/24.
+//
+
+import SwiftUI
+
+@main
+struct ClassNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClassNotesTests/ClassNotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClassNotesTests.swift
+//  ClassNotesTests
+//
+//  Created by Deepthi Munagapati on 12/28/24.
+//
+
+import Testing
+@testable import ClassNotes
+
+struct ClassNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
